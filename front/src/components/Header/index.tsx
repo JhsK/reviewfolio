c4fb49c17845fe07c4d575a13a3c41d83edc3ae5
JhsK <qwe6293@nate.com>
2022-03-28 22:28:26 +0900

    헤더 마이페이지 팝업 추가

--- front/src/components/Header/index.tsx
+++ front/src/components/Header/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import Link from 'next/link';
+import { Button, ClickAwayListener, Grow, MenuItem, MenuList, Paper, Popper } from '@mui/material';
 import useAuth from 'src/hooks/useAuth';
 import { toast, ToastContainer } from 'react-toastify';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { postLogout } from 'src/api';
+import { useRouter } from 'next/router';
 import Layout from '../Layout';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -14,7 +18,8 @@ const Container = styled.div`
   padding: 2rem 0;
   color: ${(props) => props.theme.PUBLIC_BLACK};
 
-  a {
+  a,
+  button {
     cursor: pointer;
     text-decoration: none;
     outline: none;
@@ -30,13 +35,22 @@ const Logo = styled.span`
 const MenuContainer = styled.div`
   font-size: 1.1rem;
 
-  a {
+  a,
+  button {
     margin-left: 1rem;
+  }
+
+  button {
+    font-size: 1.1rem;
+    padding: 0;
   }
 `;
 
 const Header = () => {
   const currentUser = useAuth();
+  const router = useRouter();
+  const anchorRef = React.useRef<HTMLButtonElement>(null);
+  const [open, setOpen] = React.useState(false);
 
   const NotLoggedInClick = () =>
     toast.warn('로그인이 필요합니다', {
@@ -48,6 +62,46 @@ const Header = () => {
       draggable: true,
       progress: undefined,
     });
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event: Event | React.SyntheticEvent) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target as HTMLElement)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event: React.KeyboardEvent) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === 'Escape') {
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current!.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  const onClickLogout = async () => {
+    try {
+      await postLogout();
+      router.reload();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
@@ -76,6 +130,50 @@ const Header = () => {
               <Link href="/user/mypage">
                 <a>마이페이지</a>
               </Link>
+              <Button
+                ref={anchorRef}
+                id="composition-button"
+                aria-controls={open ? 'composition-menu' : undefined}
+                aria-expanded={open ? 'true' : undefined}
+                aria-haspopup="true"
+                onClick={handleToggle}
+              >
+                마이페이지 <MdKeyboardArrowDown size="1.5rem" />
+              </Button>
+              <Popper
+                open={open}
+                anchorEl={anchorRef.current}
+                role={undefined}
+                placement="bottom-start"
+                transition
+                disablePortal
+              >
+                {({ TransitionProps, placement }) => (
+                  <Grow
+                    {...TransitionProps}
+                    style={{
+                      transformOrigin: placement === 'bottom-start' ? 'left top' : 'left bottom',
+                    }}
+                  >
+                    <Paper>
+                      <ClickAwayListener onClickAway={handleClose}>
+                        <MenuList
+                          autoFocusItem={open}
+                          id="composition-menu"
+                          aria-labelledby="composition-button"
+                          // onKeyDown={handleListKeyDown}
+                        >
+                          <MenuItem onClick={handleClose}>{`${
+                            currentUser.data.position === 'programmer' ? '신청현황' : '요청내역'
+                          }`}</MenuItem>
+                          <MenuItem onClick={handleClose}>내 정보</MenuItem>
+                          <MenuItem onClick={onClickLogout}>로그아웃</MenuItem>
+                        </MenuList>
+                      </ClickAwayListener>
+                    </Paper>
+                  </Grow>
+                )}
+              </Popper>
             </>
           ) : (
             <>
